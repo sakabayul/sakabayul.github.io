98188974bb705787e6bf868bf44dff690e036434
dahlan77 <dahlanelka@gmail.com>
2024-09-13 23:21:31 +0700

    [DEPLOY]

--- README.md
+++
@@ -1,9 +0,0 @@
-# Website My Portfolio
-
-Design UI/UX By: Saka Bayu <br>
-Web Development By: Saka Bayu
-<br>
-<br>
-Contact me: <br>
-Linkedin: https://www.linkedin.com/in/saka-bayu-l <br>
-GitHub: https://github.com/sakabayul
--- js/script.js
+++
@@ -1,8 +0,0 @@
-var loadingScreen = document.getElementById("loading-screen");
-var mainContent = document.getElementById("main-content");
-
-// JavaScript untuk mengelola pemuatan konten
-window.addEventListener('load', function () {
-    loadingScreen.style.display = 'none';
-    mainContent.style.display = 'block';
-});
